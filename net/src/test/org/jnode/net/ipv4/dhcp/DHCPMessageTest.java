367db6a2f176c07b562ef132cbd338697235820d
flesire <fabien.lesire@gmail.com>
2012-11-08 15:20:18 +0100

    Fix regression on socket buffer and add basic test for DHCP message

---
+++ net/src/test/org/jnode/net/ipv4/dhcp/DHCPMessageTest.java
@@ -0,0 +1,21 @@
+package org.jnode.net.ipv4.dhcp;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.net.DatagramPacket;
+
+import org.jnode.net.ipv4.bootp.BOOTPHeader;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class DHCPMessageTest {
+
+	@Test
+	public void testToDatagramPacket() {
+		BOOTPHeader header = Mockito.mock(BOOTPHeader.class);
+		DHCPMessage message = new DHCPMessage(header, DHCPMessage.DHCPDISCOVER);
+		DatagramPacket packet = message.toDatagramPacket();
+		assertNotNull(packet);
+	}
+
+}
